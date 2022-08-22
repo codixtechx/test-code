9de4b331d4cbb9ea2900564874b75bab198ca887
codixtech <codixtechsolution@gmail.com>
2022-08-22 19:45:10 -0400

    second working code

--- app.sh
+++ app.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 read -p "Please enter your name : " user_name
+read -p "Please enter your prefer name : " prefer_name
 
 str_build="Hello $user_name"
+
 echo $str_build
+
+echo " you gave me $prefer_name as your prefer name "
+
+sleep 10
+
+echo " how will you like me to call you ?? "
